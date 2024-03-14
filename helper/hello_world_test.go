62607dbfa14645dd191cd91a85866eb0aa302e76
Thomzes <thomz161@gmail.com>
2024-03-14 21:15:16 +0700

    feat: create benchmark using 2 func in helper package

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkHelloWorld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Thomas")
+	}
+}
+
+func BenchmarkHelloWorldArdiansah(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Ardiansah")
+	}
+}
+
 func TestTableHelloWorld(t *testing.T) {
 	tests := []struct {
 		name string
