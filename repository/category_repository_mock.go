a8b35924ad7eac1fee99c913b7f74e841ed301e3
Tommy <thomz161@gmail.com>
2024-03-14 16:27:10 +0700

    feat: create mock testing (progress)

---
+++ repository/category_repository_mock.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"go-unit-test/entity"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type CategoryRepositoryMock struct {
+	Mock mock.Mock
+}
+
+func (repository *CategoryRepositoryMock) FindById(id string) *entity.Category {
+
+}
