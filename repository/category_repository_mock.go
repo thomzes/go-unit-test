94e7a3992e0dea9ee6421810771711b1c05f348d
Thomzes <thomz161@gmail.com>
2024-03-14 21:32:04 +0700

    modified:   go.mod 	modified:   go.sum 	modified:   repository/category_repository_mock.go

--- repository/category_repository_mock.go
+++ repository/category_repository_mock.go
@@ -11,5 +11,11 @@ type CategoryRepositoryMock struct {
 }
 
 func (repository *CategoryRepositoryMock) FindById(id string) *entity.Category {
-
+	arguments := repository.Mock.Called(id)
+	if arguments.Get(0) == nil {
+		return nil
+	}else {
+		category := arguments.Get(0).(entity.Category)
+		return &category
+	}
 }
